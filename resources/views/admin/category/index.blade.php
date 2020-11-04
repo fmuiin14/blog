0c92efda237d9e72c289859fa5d291efae5cf996
fmuiin14 <fmuiin14@gmail.com>
2020-11-04 20:47:07 +0700

    pagination 4

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>ini adalah category</h1>
-    @foreach ($categories as $category)
+    
         <table class="table table-striped table-hover table-sm table-bordered">
             <thead>
                 <tr>
@@ -19,16 +19,19 @@
             </tr>
             </thead>
             <tbody>
+                @foreach ($categories as $category => $hasil)
                 <tr>
-                    <td></td>
-                <td>{{$category->name}}</td>
+                    <td>{{$category + $categories->firstitem()}}</td>
+                <td>{{$hasil->name}}</td>
                 <td>
                     <a href="#" class="btn btn-primary btn-sm">Edit</a>    
                     <a href="#" class="btn btn-danger btn-sm">Delete</a>    
                 </td>
                 </tr>
+                @endforeach
             </tbody>
         </table>
-    @endforeach
+
+        {{$categories->links()}}
 
 @endsection
