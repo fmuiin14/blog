1419b92da90f9aca1a46b60e4f7437828fc8b394
fmuiin14 <fmuiin14@gmail.com>
2020-11-07 20:58:59 +0700

    8 category delete & flashdata

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -104,6 +104,9 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return redirect()->back()->with('hapus', 'Data Berhasil Di Hapus');
     }
 }
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -10,6 +10,10 @@ Kategori
 <div class="alert alert-primary" role="alert">
     {{Session('success')}}
 </div>
+@elseif (Session::has('hapus'))
+<div class="alert alert-danger" role="alert">
+    {{Session('hapus')}}
+</div>
 @endif
 
 <a href="{{route('category.create')}}" class="btn btn-info btn-sm mb-3">Tambah Kategori</a>
@@ -35,8 +39,14 @@ Kategori
             <td>{{$category + $categories->firstitem()}}</td>
             <td>{{$hasil->name}}</td>
             <td>
-                <a href="{{route('category.edit', $hasil->id)}}" class="btn btn-primary btn-sm">Edit</a>
-                <a href="{{ route('category.destroy', $hasil->id) }}" class="btn btn-danger btn-sm">Delete</a>
+
+                <form action="{{ route('category.destroy', $hasil->id) }}" method="POST">
+                    @csrf
+                    @method('delete')
+                    <a href="{{route('category.edit', $hasil->id)}}" class="btn btn-primary btn-sm">Edit</a>
+                    <button type="submit" href="{{ route('category.destroy', $hasil->id) }}"
+                        class="btn btn-danger btn-sm">Delete</button>
+                </form>
             </td>
         </tr>
         @endforeach
